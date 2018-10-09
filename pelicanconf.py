4e417b882cbc9d14eb8f66e4f34773888b98e2fe
Danilo Bargen <mail@dbrgn.ch>
2018-10-09 22:42:46 +0200

    Add Dockerfile

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'SpaceAPI Contributors'
 SITENAME = 'SpaceAPI'
-SITEURL = 'https://spaceapi.io/'
+SITEURL = 'https://spaceapi.io'
 
 PATH = 'content'
 
